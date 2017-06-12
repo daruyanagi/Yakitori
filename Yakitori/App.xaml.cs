e6d7856e4efcea229ae15bf3349cb375201ed48a
Hidetoshi Yanagi <daruyanagi@gmail.com>
2017-06-13 01:35:00 +0900

    rouph coding

--- Yakitori/App.xaml.cs
+++ Yakitori/App.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows;
 
 namespace Yakitori
 {
+    using System.Xml;
+    using Windows.UI.Notifications;
     using WindowsInput;
     using WindowsInput.Native;
 
@@ -16,8 +18,7 @@ namespace Yakitori
     /// </summary>
     public partial class App : Application
     {
-        [STAThread]
-        static void Main()
+        public App()
         {
             var args = Environment.GetCommandLineArgs();
 
@@ -27,31 +28,163 @@ namespace Yakitori
 
             switch (option)
             {
-                case "/d":
-                    keybord.KeyPress(VirtualKeyCode.SNAPSHOT);
-                    break;
-                case "/a":
-                    var window = new Views.CoundDownWindow();
-
-                    window.Show(5, () => 
+                case "/d": /* デスクトップ全体を撮影 */
+                    ShowCountDown(() =>
                     {
-                        keybord.ModifiedKeyStroke(VirtualKeyCode.MENU, VirtualKeyCode.SNAPSHOT);
+                        keybord.KeyPress(VirtualKeyCode.SNAPSHOT);
+                        ShowToast("Capture is completed, and saved to clipboard.", "Desktop");
+                        App.Current.Shutdown();
                     });
                     break;
-                case "/r":
-                    var modefier_keys = new VirtualKeyCode[] 
+                case "/f": /* デスクトップ全体を撮影してファイルへ保存 */
+                    ShowCountDown(() =>
                     {
-                        VirtualKeyCode.LWIN,
-                        VirtualKeyCode.SHIFT,
-                    };
-                    keybord.ModifiedKeyStroke(modefier_keys, VirtualKeyCode.VK_S);
+                        keybord.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.SNAPSHOT);
+                        ShowToast(@"Capture is completed, and saved to ""Pictures/Screenshots"" folder.", "Desktop");
+                        App.Current.Shutdown();
+                    });
+                    break;
+                case "/a": /* アクティブウィンドウを撮影 */
+                    ShowCountDown(() =>
+                    {
+                        keybord.ModifiedKeyStroke(VirtualKeyCode.MENU, VirtualKeyCode.SNAPSHOT);
+                        ShowToast("Capture is completed, and saved to clipboard.", "Active Window");
+                        App.Current.Shutdown();
+                    });
+                    break;
+                case "/r": /* デスクトップの選択領域を撮影 */
+                    ShowCountDown(() =>
+                    {
+                        var modefier_keys = new VirtualKeyCode[]
+                        {
+                            VirtualKeyCode.LWIN,
+                            VirtualKeyCode.SHIFT,
+                        };
+                        keybord.ModifiedKeyStroke(modefier_keys, VirtualKeyCode.VK_S);
+                        App.Current.Shutdown();
+                    });
+                    break;
+                case "/s": /* クリップボードの画像をスクリーンショットフォルダーへ保存 */
+                    try
+                    {
+                        var image = Clipboard.GetImage();
+
+                        if (image != null)
+                        {
+                            var enc = new System.Windows.Media.Imaging.PngBitmapEncoder();
+                            var path = System.IO.Path.Combine(
+                                Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                                "Screenshots",
+                                DateTime.Now.ToString("yyyyMMdd-hhmmss"));
+                            path = System.IO.Path.ChangeExtension(path, "png");
+
+                            using (var stream = new System.IO.FileStream(path, System.IO.FileMode.Create))
+                            {
+                                enc.Frames.Add(System.Windows.Media.Imaging.BitmapFrame.Create(image));
+                                enc.Save(stream);
+                            }
+
+                            ShowToast("The image is saved.", "Clipboard", path, (n, o) => 
+                            {
+                                System.Diagnostics.Process.Start("explorer.exe", $@"/select,""{path}""");
+                            });
+                        }
+                        else
+                        {
+                            ShowToast("There are no images.", "Clipboard");
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        ShowToast(exception.Message, exception.GetType().Name);
+                    }
+                    finally
+                    {
+                        App.Current.Shutdown();
+                    }
+                    break;
+                case "/o":
+                    var folder = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+                    folder = System.IO.Path.Combine(folder, "Screenshots");
+                    System.Diagnostics.Process.Start("explorer.exe", folder);
+                    App.Current.Shutdown();
                     break;
                 default:
-                    var app = new App();
-                    app.InitializeComponent();
-                    app.Run();
+                    MainWindow = new Views.SettingsWindow();
+                    MainWindow.Show();
                     break;
             }
+        }
+
+        public static void ShowToast(string message, string title, string image_path = "", 
+            Windows.Foundation.TypedEventHandler<ToastNotification, object> handler = null)
+        {
+            if (!Yakitori.Properties.Settings.Default.IsNotificationEnabled) return;
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var app_name = assembly.GetName().Name;
+            var app_path = System.IO.Path.GetDirectoryName(assembly.Location);
+            
+            image_path = string.IsNullOrEmpty(image_path)
+                ? "file:///" + System.IO.Path.Combine(app_path, "app.png")
+                : "file:///" + image_path;
+
+            // Get a toast XML template
+            var xml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText04);
+
+            // Text
+            var text_elements = xml.GetElementsByTagName("text");
+            text_elements[0].AppendChild(xml.CreateTextNode(title));
+            text_elements[1].AppendChild(xml.CreateTextNode(message));
+
+            // Image
+            var image_elements = xml.GetElementsByTagName("image");
+            image_elements[0].Attributes.GetNamedItem("src").NodeValue = image_path;
+
+            // Audio
+            var audio_element = xml.CreateElement("audio");
+            audio_element.SetAttribute("src", "ms-winsoundevent:Notification.Default");
+            audio_element.SetAttribute("loop", "false");
+            xml.DocumentElement.AppendChild(audio_element);
+
+            var toast = new ToastNotification(xml);
+
+            if (handler != null) toast.Activated += handler;
+            
+            ToastNotificationManager.CreateToastNotifier(app_name).Show(toast);
+        }
+
+        public static void ShowCountDown(Action action)
+        {
+            if (!Yakitori.Properties.Settings.Default.IsCountDownEnabled)
+            {
+                action();
+
+                return;
+            }
+
+            var count = Yakitori.Properties.Settings.Default.Count;
+            var window = new Views.CountDownWindow();
+            var timer = new System.Windows.Threading.DispatcherTimer();
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += (sender, args) =>
+            {
+                window.CountDownLabel.Text = count.ToString();
+
+                count = count - 1;
+
+                if (count < 0)
+                {
+                    window.Close();
+
+                    action();
+                }
+            };
+
+            window.Loaded += (sender, args) => timer.Start();
+
+            window.Show();
         }
     }
 }
---
+++ Yakitori/ViewModels/SettingsWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yakitori.ViewModels
+{
+    public class SettingsWindowViewModel : BindableBase
+    {
+        ~SettingsWindowViewModel()
+        {
+            Yakitori.Properties.Settings.Default.Save();
+        }
+
+        public string Title
+        {
+            get
+            {
+                var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                var assembly_info = assembly.GetName();
+
+                return $"{assembly_info.Name} {assembly_info.Version}";
+            }
+        }
+    }
+}
